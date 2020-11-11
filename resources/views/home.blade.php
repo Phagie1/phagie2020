443359f335e2a8d196c778726b82bae2457beeaf
Jayeblade <jaypodador@gmail.com>
2020-11-11 23:24:25 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <div class="jumbotron jumbotron-fluid">
   <div class="container" style="text-align:center;">
-    <h1 class="display-3">University of Oxford</h1>
+    <h1 class="display-3"style="color: blue;">University of Welcome Delegates</h1>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
   </div>
 </div>
@@ -12,19 +12,19 @@
 <div class="row">
 <div class="col md-4">
     <div class="h5">
-        <h1 style="text-align:center;"> Vision</h1>
+        <h1 style="text-align:center;"> Study</h1>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quia fugit aliquid molestias minus doloribus facere neque ab delectus, aperiam consectetur, ipsa sit vitae, tempora officiis ullam porro perferendis dolores!
     </div>
 </div>
 <div class="col md-4">
     <div class="h5">
-        <h1 style="text-align:center;">Mission</h1>
+        <h1 style="text-align:center;">Pray</h1>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quia fugit aliquid molestias minus doloribus facere neque ab delectus, aperiam consectetur, ipsa sit vitae, tempora officiis ullam porro perferendis dolores!
     </div>
 </div>
 <div class="col md-4">
     <div class="h5">
-        <h1 style="text-align:center;">Goal</h1>
+        <h1 style="text-align:center;">Loved</h1>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quia fugit aliquid molestias minus doloribus facere neque ab delectus, aperiam consectetur, ipsa sit vitae, tempora officiis ullam porro perferendis dolores!
     </div>
 </div>
